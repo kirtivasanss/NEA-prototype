260177aa1ea6a8249097f14c8bcec11519bd20d1
kirtivasanss <kirtivasans@gmail.com>
2025-02-13 12:50:05 +0000

    Day FINSIHED

--- main.py
+++ main.py
@@ -4,11 +4,17 @@ from signupPage import signupPage
 from viewpage import viewPage
 from search_page import searchPage
 from account_page import accountPage
+from database_operations import create_tables,create_connection
+
+connection = create_connection
+cursor = connection.cursor()
+
+create_tables(cursor)
 
 pages = st.navigation(
     [
-        st.Page(signupPage, title="Login"),
-        st.Page(viewPage,title="View Candidates"),
+       st.Page(signupPage, title="Login"),
+       st.Page(viewPage,title="View Candidates"),
        st.Page(addPage,title="Add Candidates"),
        st.Page(searchPage,title="Search Page"),
        st.Page(accountPage,title="Account Page")
